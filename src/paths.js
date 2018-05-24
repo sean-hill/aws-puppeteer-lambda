a85f3fc771526ecc413f56deb86c5a3d9585ed6f
Sean Hill <sean.daryl.hill@gmail.com>
2018-05-24 11:04:41 -0700

    Support for puppeteer 1.4.0.

--- src/paths.js
+++ src/paths.js
@@ -3,6 +3,9 @@ const TEMP_PATH = process.env.TMP_PATH || '/tmp'
 
 module.exports = {
   TMP: TEMP_PATH,
-  COMPRESSED_EXECUTABLE: path.resolve(__dirname, './headless_shell_r549031.zip'),
+  COMPRESSED_EXECUTABLE: path.resolve(
+    __dirname,
+    './revisions/headless_shell_r555668.zip'
+  ),
   EXECUTABLE: path.resolve(TEMP_PATH, './headless_shell')
 }
